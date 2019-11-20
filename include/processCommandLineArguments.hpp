3e35243ce2eefe1c3a03903b1fa655f476aa98f0
tom-robin teschner <tomrobin.teschner@gmail.com>
2019-11-20 22:21:55 +0100

    class added for command line arguments
    command line arguments can be passed to the parser
    which are then evaluated and stored inside the class.
    Alternatively, it can be used to print the help contect
    to screen.

---
+++ include/processCommandLineArguments.hpp
@@ -0,0 +1,46 @@
+#ifndef PROCESSCOMMANDLINEARGUMENTS_HPP
+#define PROCESSCOMMANDLINEARGUMENTS_HPP
+
+#include <iostream>
+#include <string>
+
+class processCommandLineArguments {
+
+  public:
+    processCommandLineArguments(unsigned numArguments, char **arguments) : _numArguments(numArguments),
+      _arguments(arguments)
+    {
+      // check if the user only wants to see the help section
+      if (_numArguments > 1)
+      {
+        if (std::string(_arguments[1]) == "-h" || std::string(_arguments[1]) == "--help")
+        {
+          printHelp();
+          std::exit(0);
+        }
+      }
+      setArguments();
+    }
+
+    /// get the path and name of the json file
+    std::string getStyleFile() const;
+
+  private:
+    /// print help to screen
+    void printHelp();
+
+    /// store the command line arguments
+    void setArguments();
+
+  private:
+    /// number of command line arguments passed to program
+    const unsigned _numArguments;
+
+    /// actual command line arguments
+    char **_arguments;
+
+    /// path and filename of JSON file
+    std::string _jsonFile = "";
+};
+
+#endif
