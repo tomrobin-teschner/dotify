6f4ed20085f43ea888274c4f60bdd65664557a9f
tom-robin teschner <tomrobin.teschner@gmail.com>
2019-11-21 22:38:11 +0100

    Added the line parser class
    This class allows to modify a given string which is
    triggered by a keyword, which has styling
    parameters associated with it.

---
+++ include/lineParser.hpp
@@ -0,0 +1,83 @@
+#ifndef LINEPARSER_HPP
+#define LINEPARSER_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+#include <tuple>
+
+#include "include/jsonParser.hpp"
+#include "include/styling.hpp"
+
+/// this class checks a if a given line requires formating and returns the final string that should be printed to screen
+class lineParser
+{
+  public:
+    lineParser(const nlohmann::json &jsonArgs) : _jsonArgs(jsonArgs)
+    {
+      for (const auto entry : _jsonArgs["ignoreCompleteLineIfItContainsSubstring"])
+        _ignoreLineContainingString.push_back(entry);
+
+      for (const auto entry : _jsonArgs["styleLineContainingKeywords"])
+      {
+        assert(entry.count("keyword") != 0 && "No keyword defined in styleLineContainingKeywords");
+        assert(entry.count("applyStyle") != 0 && "No style defined in styleLineContainingKeywords");
+
+        const std::string keyword = entry["keyword"];
+        const bool gatherStatistics = entry.count("gatherStatistics") == 0 ? false :
+          static_cast<const bool>(entry["gatherStatistics"]);
+        const std::string applyStyle = entry["applyStyle"];
+        const std::string color = entry.count("color") == 0 ? std::string("white") :
+          static_cast<const std::string>(entry["color"]);
+        const std::string style = entry.count("style") == 0 ? std::string("normal") :
+          static_cast<const std::string>(entry["style"]);
+
+        _keywordToStyle.push_back(styling(keyword, gatherStatistics, applyStyle, color, style));
+      }
+
+      for (const auto entry : _jsonArgs["ignoreContentBetweenDelimiter"])
+      {
+        assert(entry.count("startingDelimiter") != 0 && "No delimiter defined in ignoreContentBetweenDelimiter");
+        assert(entry.count("endingDelimiter") != 0 && "No delimiter defined in ignoreContentBetweenDelimiter");
+        const std::string start = entry["startingDelimiter"];
+        const std::string end = entry["endingDelimiter"];
+        const std::string replaceBy = entry.count("replaceContentBy") == 0 ? std::string("...") :
+          static_cast<const std::string>(entry["replaceContentBy"]);
+
+        _delimiters.push_back(std::make_tuple(start, end, replaceBy));
+      }
+    }
+
+    /// check if the current line contains strings that should not be printed to screen
+    bool checkIfStringShouldBeSurpressed(std::string &line) const;
+
+    /// check if any delimiter is contained within the current line
+    void checkForAnyDelimiterInCurrentLine(std::string &line) const;
+
+    /// check if the current line requires formating
+    void checkIfStringRequiresStyleAdjustment(std::string &line) const;
+
+  private:
+    /// check if the current line contains delimiter between which the content is supposed to be removed
+    void removeContentBetweenDelimiter(const std::string &startDelimiter, const std::string &endDelimiter,
+      const std::string &replaceBy, std::string &line) const;
+
+    /// apply color and formating to line
+    void applyLineFormating(const styling &style, std::string &line) const;
+
+  private:
+    /// the json object containing all required styling information
+    const nlohmann::json &_jsonArgs;
+
+    /// vector that holds all strings which, if present, will trigger a line to be surpressed from the output
+    std::vector<std::string> _ignoreLineContainingString;
+
+    /// vector that specifies which keywords to format and style as well as how to
+    std::vector<styling> _keywordToStyle;
+
+    /// vector containing the delimiters between which the content is reduced to a user-defined string
+    std::vector<std::tuple<std::string, std::string, std::string>> _delimiters;
+};
+
+#endif
---
+++ src/lineParser.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cassert>
+
+#include "include/lineParser.hpp"
+
+bool lineParser::checkIfStringShouldBeSurpressed(std::string &line) const
+{
+  bool surpressString = false;
+  for (const auto entry : _ignoreLineContainingString)
+    if (line.find(entry) != std::string::npos)
+      surpressString = true;
+  return surpressString;
+}
+
+void lineParser::checkForAnyDelimiterInCurrentLine(std::string &line) const
+{
+  for (const auto delimiterTuple : _delimiters)
+  {
+    const auto start = std::get<0>(delimiterTuple);
+    const auto end = std::get<1>(delimiterTuple);
+    const auto replaceBy = std::get<2>(delimiterTuple);
+    removeContentBetweenDelimiter(start, end, replaceBy, line);
+  }
+}
+
+void lineParser::checkIfStringRequiresStyleAdjustment(std::string &line) const
+{
+  for (const auto entry : _keywordToStyle)
+    if (line.find(entry.getKeyword()) != std::string::npos)
+      applyLineFormating(entry, line);
+}
+
+void lineParser::removeContentBetweenDelimiter(const std::string &startDelimiter, const std::string &endDelimiter,
+  const std::string &replaceBy, std::string &line) const
+{
+  // replace types by <...>
+  long unsigned pos = 0;
+  long unsigned length = 0;
+  int count = 0;
+  bool containsDelimiters = false;
+  long unsigned i = 0;
+  long unsigned lineLength = line.length();
+  for (i = 0; i < lineLength; ++i) {
+    if (line.substr(i, startDelimiter.length()) == startDelimiter)
+    {
+      if (!containsDelimiters)
+      {
+        pos = i;
+        containsDelimiters = true;
+      }
+      count++;
+    }
+    if (line.substr(i, endDelimiter.length()) == endDelimiter)
+      count--;
+    if (containsDelimiters && count == 0)
+    {
+      length = i - pos + 1;
+      line = line.replace(pos, length, startDelimiter + replaceBy + endDelimiter);
+      containsDelimiters = false;
+      i = pos + startDelimiter.length() + replaceBy.length() + endDelimiter.length();
+      lineLength = line.length();
+    }
+  }
+}
+
+void lineParser::applyLineFormating(const styling &style, std::string &line) const
+{
+  const auto keyword = style.getKeyword();
+  const auto applyAtLocation = style.getStylingLocation();
+  const auto color = style.getColor();
+  const auto lineStyle = style.getStyle();
+
+  assert((applyAtLocation == std::string("preKeyword") || applyAtLocation == std::string("onKeyword") ||
+    applyAtLocation == std::string("postKeyword") || applyAtLocation == std::string("everywhere")) &&
+    "Could not determine where to apply line formating.");
+
+  const auto position = line.find(keyword);
+  const auto length = keyword.length();
+
+  if (position != std::string::npos)
+  {
+    if (applyAtLocation == std::string("preKeyword"))
+      line = lineStyle + color + line.substr(0, position) + format::NORMAL + color::WHITE + line.substr(position);
+    else if (applyAtLocation == std::string("onKeyword"))
+      line = line.substr(0, position) + lineStyle + color + keyword + format::NORMAL + color::WHITE +
+        line.substr(position + length);
+    else if (applyAtLocation == std::string("postKeyword"))
+      line = line.substr(0, position + length) + lineStyle + color + line.substr(position + length) + format::NORMAL +
+        color::WHITE;
+    else if (applyAtLocation == std::string("everywhere"))
+      line = color + lineStyle + line + format::NORMAL + color::WHITE;
+  }
+  // auto pos   = line.find("Processing");
+  // int length = 10;
+  // if (pos != std::string::npos) {
+  //   line = line.substr(0, pos + length) + BLUE + BOLD + line.substr(pos + length, line.size() - (pos + length)) + NORMAL + WHITE;
+  //   returnValue = OK;
+  // }
+
+  // pos = line.find("Waf: Entering directory");
+  // if (pos != std::string::npos) {
+  //   line = GREEN + line + WHITE;
+  //   returnValue = OK;
+  // }
+}
